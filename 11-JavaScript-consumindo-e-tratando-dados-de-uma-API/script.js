9dd1325d095448ea4f46eba622c472c503d51cdb
Raquel Rios <raquel.nmr14@gmail.com>
2024-05-04 14:18:24 -0300

    atualizando para a correção

--- 11-JavaScript-consumindo-e-tratando-dados-de-uma-API/script.js
+++ 11-JavaScript-consumindo-e-tratando-dados-de-uma-API/script.js
@@ -4,7 +4,7 @@ const api = fetch("http://localhost:3000/videos") // fazer uma busca na api de v
 //console.log(api); // só pra verificar se a promessa no console.log está no status "fullfiled"
 //then(res => console.log(res.json)); 
 .then(res => res.json())
-.then((videos) => {
+.then((videos) => 
     videos.forEach((video) => {
         containerVideos.innerHTML += `
         <li class="videos__item">
@@ -12,10 +12,9 @@ const api = fetch("http://localhost:3000/videos") // fazer uma busca na api de v
             <div class="descricao-video">
                 <img class="img-canal" src="${video.imagem}" alt="logo no canal">
                 <h3 class="titulo-video">${video.titulo}</h3> 
-                <p class="titulo-canal">${video.descricao }</p>
+                <p class="titulo-canal">${video.descricao}</p>
             </div>
-
         </li>
         `;    
-    });
-});
+    })
+)
