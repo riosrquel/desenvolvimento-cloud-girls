818f4da5a67c133440fbd4205d407247739aaf39
Raquel Rios <raquel.nmr14@gmail.com>
2024-05-04 17:28:35 -0300

    correção aula 11

--- 11-JavaScript-consumindo-e-tratando-dados-de-uma-API/script.js
+++ 11-JavaScript-consumindo-e-tratando-dados-de-uma-API/script.js
@@ -1,20 +1,93 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos") // fazer uma busca na api de vídeos
-//console.log(api); // só pra verificar se a promessa no console.log está no status "fullfiled"
-//then(res => console.log(res.json)); 
+const api = fetch("http://localhost:3000/videos")
 .then(res => res.json())
 .then((videos) => 
-    videos.forEach((video) => {
+    videos.forEach((video)=> {
         containerVideos.innerHTML += `
         <li class="videos__item">
-            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+            <iframe src="${video.url}" title"${video.titulo}" frameborder="0" allowfullscreen></iframe>
             <div class="descricao-video">
-                <img class="img-canal" src="${video.imagem}" alt="logo no canal">
+                <img class="img-canal" src"${video.imagem} alt="Logo do Canal">
                 <h3 class="titulo-video">${video.titulo}</h3> 
                 <p class="titulo-canal">${video.descricao}</p>
             </div>
+
         </li>
-        `;    
+        `;  
+
     })
 )
+/*.catch((error) => {
+    containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`
+})
+/*async function buscarEMostrarVideos() {
+    try {
+    const busca = await fetch("http://localhost:3000/videos")
+    const videos = await busca.json();
+    
+        videos.forEach((video)=> {
+            if(video.categoria =="") { // para criar erros personalizados para os erros aparecerem
+                throw new Error('Video não tem categoria');
+            }
+            containerVideos.innerHTML += `
+            <li class="videos__item">
+                <iframe src="${video.url}" title"${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img class="img-canal" src"${video.imagem} alt="Logo do Canal">
+                    <h3 class="titulo-video">${video.titulo}</h3> 
+                    <p class="titulo-canal">${video.descricao}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
+                </div>
+            </li>
+            `;  
+        })
+    } catch(error) {
+        containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`
+    /*}finally {
+        alert('Isso sempre acontece')
+    }
+    }  
+}  
+
+        
+buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector(".psquisa__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll(".videos__item");
+        if(barraDePesquisa.valeu != "") {
+            for (let video of videos) {
+                let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+                let valorFiltro = barraDePesquisa.value.toLowerCase();
+                
+               if(!titulo.includes(valorFiltro)) {
+                    video.style.display = "none";                
+               }
+               else{
+                video.style.display = "block"; 
+               }
+            }
+        } else{
+            video.style.display = "block";
+        }
+}
+
+const botaoCategoria = documento.querySelectorAll(".superior__item");
+    botaoCategoria.forEach((botao) => {
+        let nomeCategoria = botao.getAttribute("name"); 
+        botao.addEventListener('click', () => filtrarPorCategoria(nomeCategoria));
+    })
+
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item");
+    for (let video of videos) {
+        let categoria = video.querySelector (".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'Tudo')
+    }   
+}*/
